d9727390ba06a649495962873d2ee4d202823ed7
Tomas Jerry Sebo <sebo.tomas@gmail.com>
2017-12-25 17:05:21 +0100

    Quote object inicialization

--- js/script.js
+++ js/script.js
@@ -2,3 +2,44 @@
 // when user clicks anywhere on the button, the "printQuote" function is called
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
 
+// initial object initialization of quotes
+
+var quotes = [
+	{ 	"quote":"The mind is everything. What you think you become.",
+		"source":"Buddha",
+		"citation":"",
+		"year":""
+	},
+	{ 	"quote":"The best time to plant a tree was 20 years ago. The second best time is now.",
+		"source":"Chinese Proverb",
+		"citation":"",
+		"year":"1400"
+	},
+	{ 	"quote":"I'm your father",
+		"source":"Darth Vader",
+		"citation":"Star Wars Episode V",
+		"year":"1980"
+	},
+	{ 	"quote":"It's not who i'm underneath, but what i do that defines me.",
+		"source":"Christian Bale",
+		"citation":"Batman, Batman Begins",
+		"year":"2008"
+	},
+	{ 	"quote":"Your time is limited, so don’t waste it living someone else’s life.",
+		"source":"Steve Jobs",
+		"citation":"",
+		"year":"2005"
+	},
+	{ 	"quote":"Winning isn’t everything, but wanting to win is.",
+		"source":"Vince Lombardi",
+		"citation":"",
+		"year":""
+	}
+];
+
+function printQuote() {
+	return true;
+}
+
+console.log(quotes);
+

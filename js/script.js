73f4e840d50aa35a2103501599e9d58f9beb63da
Tomas Jerry Sebo <sebo.tomas@gmail.com>
2017-12-25 21:54:16 +0100

    Project finisher v. 1.0, random quote generator, random hex- color generator, quote swap and background color change on button click, automatic quote swapper, documentation

--- js/script.js
+++ js/script.js
@@ -1,9 +1,12 @@
 // event listener to respond to "Show another quote" button clicks
 // when user clicks anywhere on the button, the "printQuote" function is called
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
+document.getElementById('loadQuoteAuto').addEventListener("click", quoteAutoChange, false);
 
-// initial object initialization of quotes
+// initialize the intervalID variable for the setInterval function
+var intervalID = 0;
 
+// initialize the quote object
 var quotes = [
 	{ 	"quote":"The mind is everything. What you think you become.",
 		"source":"Buddha",
@@ -37,9 +40,51 @@ var quotes = [
 	}
 ];
 
-function printQuote() {
-	return true;
+// function to generate a random  HEX number for changing the backround color
+function getRandomColor () {
+  var hex = Math.floor(Math.random() * 0xFFFFFF); // generating a number between 
+  return "#" + ("000000" + hex.toString(16)).substr(-6); // hexidecimal conversion and cutting off the 6 "0000000" at the beginning
 }
 
-console.log(quotes);
+// generating a random index for the quotes object by generating a integer between 0 and the length of the object
+function getRandomQuote(){
+	var quoteIndex =  Math.floor(Math.random() * quotes.length);
+	return quotes[quoteIndex];
+}
+
+// function to start automatically swapping the quotes
+function quoteAutoChange() {
+	// stop or start the interval swap
+ 	if(intervalID > 0) {  clearInterval(intervalID); intervalID = 0; alert("Auto swap disabled.")}
+ 	else {
+ 		// change quote asap as the button is clicked for starting the autoswap
+		printQuote();
+		// start the atomatic quote change every 5 seconds
+		 intervalID = setInterval(function(){
+			document.getElementById('loadQuote').click();
+		},5000);
+	}
+}
+
+// creating the quote string and passing it to the specified element and changing the backround color of the body element
+function printQuote() {
+	// changer the backround color 
+	document.body.style.backgroundColor = getRandomColor();
+	// call function to generate random quote
+	var randomQuote = getRandomQuote(quotes);
+	// construction the quote string
+	var quote = '<p class="quote">'+randomQuote.quote+'</p>';
+	quote += '<p class="source">'+randomQuote.source;
+	// only adding the citation if not empty
+	quote += (randomQuote.citation.length > 0) ? '<span class="citation">'+randomQuote.citation+'</span>' : '';
+	// only adding the year if not empty
+	quote += (randomQuote.year.length > 0) ? '<span class="year">'+randomQuote.year+'</span>' : '';
+	quote += '</p>';
+	// appending the string to the inner html of the targeted element
+	document.getElementById('quote-box').innerHTML = quote;
+}
+
+
+
+
 
